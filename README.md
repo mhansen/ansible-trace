b64ce5e2724deda343d16ce94d7c5462ff2627ba
Mark Hansen <mark@markhansen.co.nz>
2021-10-08 21:14:56 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# ansible-trace
+
+Visualise where time spent in your Ansible playbooks: what tasks, and what hosts, so you can find where to optimise and decrease playbook latency.
+
+An Ansible [Callback Function](https://docs.ansible.com/ansible/latest/plugins/callback.html) which traces the execution time of Ansible playooks, outputting [Chrome's Trace Event Format](https://docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/preview) for visualising in the [Perfetto](https://ui.perfetto.dev/) in-browser trace UI.
+
+Here's a trace of me deploying to my home Raspberry Pi cluster, with the default [`strategy: linear`](https://docs.ansible.com/ansible/latest/collections/ansible/builtin/linear_strategy.html#linear-strategy). You can see that now all the tasks are synchronized across hosts, with each host waiting for the slowest host before proceeding to the next task:
+
+![Perfetto window showing tasks all happening synchronized](ansible-trace-lockstep.png)
+
+Here's the same playbook ran with [`strategy: free`](https://docs.ansible.com/ansible/latest/collections/ansible/builtin/free_strategy.html) so fast hosts run to completion without waiting for slow hosts:
+
+![Perfetto window showing durations](ansible-trace-marked-up.png)
+
+You can click on tasks to see details about them:
+
+![Perfetto window showing details showing arguments and filename of task](ansible-trace-slice-details.png)
+
+## Usage
+
+1.  Copy `trace.py` into your Ansible's `callback_plugins` directory, or in [other positions Ansible accepts](https://docs.ansible.com/ansible/latest/plugins/callback.html#enabling-callback-plugins), e.g.:
+
+    ```
+    ansible-root
+    ├── ansible.cfg
+    ├── site.yml
+    └── callback_plugins
+        └── trace.py
+    ```
+
+1.  Enable the `trace` callback plugin in your `ansible.cfg`:
+
+    ```ini
+    [defaults]
+    callback_enabled = trace
+    ```
+
+    Or, enable it at the top of your playbook yml:
+
+    ```yml
+    ansible:
+      env:
+        CALLBACKS_ENABLED: trace
+        TRACE_OUTPUT_DIR: .
+        TRACE_HIDE_TASK_ARGUMENTS: True
+    ```
+
+1.  Run your Ansible Playbook:
+
+    ```shell
+    $ ansible-playbook site.yml
+    ```
+
+    This will output `trace.json` in the `TRACE_OUTPUT_DIR`, defaulting to your current working directory.
+
+1.  Open https://ui.perfetto.dev/, and drag-and-drop in the `trace.json`.
+    
+    You don't have to wait for the trace to finish; you can open in-progress trace files.
---
+++ trace.py
@@ -0,0 +1,144 @@
+DOCUMENTATION = '''
+    name: trace
+    type: aggregate
+    short_description: write playbook output to Chrome's Trace Event Format file
+    description:
+      - This callback writes playbook output to Chrome Trace Event Format file.
+    author: Mark Hansen (@mhansen)
+    options:
+      trace_output_dir:
+        name: output dir
+        default: .
+        description: Directory to write files to.
+        env:
+          - name: TRACE_OUTPUT_DIR
+      hide_task_arguments:
+        name: Hide the arguments for a task
+        default: False
+        description: Hide the arguments for a task
+        env:
+          - name: HIDE_TASK_ARGUMENTS
+    requirements:
+      - enable in configuration
+'''
+
+import json
+import os
+import time
+from dataclasses import dataclass
+
+from ansible.plugins.callback import CallbackBase
+
+
+class CallbackModule(CallbackBase):
+    """
+    This callback traces execution time of tasks to Trace Event Format.
+
+    This plugin makes use of the following environment variables:
+        TRACE_OUTPUT_DIR (optional): Directory to write JSON files to.
+                                     Default: .
+        TRACE_HIDE_TASK_ARGUMENTS (optional): Hide the arguments for a task
+                                     Default: False
+    """
+
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'aggregate'
+    CALLBACK_NAME = 'trace'
+    CALLBACK_NEEDS_ENABLED = True
+
+    def __init__(self):
+        super(CallbackModule, self).__init__()
+
+        self._output_dir = os.getenv('TRACE_OUTPUT_DIR', os.path.expanduser('.'))
+        self._hide_task_arguments = os.getenv('TRACE_HIDE_TASK_ARGUMENTS', 'False').lower()
+        self._hosts = {}
+        self._next_pid = 1
+
+        if not os.path.exists(self._output_dir):
+            os.makedirs(self._output_dir)
+        output_file = os.path.join(self._output_dir, 'trace.json')
+        self._f = open(output_file, 'w')
+        self._f.write("[\n")
+
+    def _write_event(self, e):
+            json.dump(e, self._f)
+            self._f.write(",\n")
+            self._f.flush()
+
+    def v2_runner_on_start(self, host, task):
+        uuid = task._uuid
+        name = task.get_name().strip()
+
+        args = None
+        if not task.no_log and self._hide_task_arguments == 'false':
+            args = task.args
+
+        host_uuid = host._uuid
+        if host_uuid not in self._hosts:
+            pid = self._next_pid
+            self._hosts[host_uuid] = Host(pid=pid, name=host.name)
+            self._next_pid += 1
+            # https://docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/preview#bookmark=id.iycbnb4z7i9g
+            self._write_event({
+                "name": "process_name",
+                "pid": pid,
+                "ph": "M",
+                "args": {
+                    "name": host.name,
+                },
+            })
+
+        # See "Duration Events" in:
+        # https://docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/preview#heading=h.nso4gcezn7n1
+        self._write_event({
+            "name": name,
+            "cat": "runner",
+            "ph": "B",  # Begin
+            "ts": time.time_ns() / 1000,
+            "pid": self._hosts[host_uuid].pid,
+            "id": abs(hash(uuid)),
+            "args": {
+                "args": args,
+                "task": name,
+                "path": task.get_path(),
+                "host": host.name,
+            },
+        })
+
+
+    def _end_span(self, result, status):
+        task = result._task
+        uuid = task._uuid
+        # See "Duration Events" in:
+        # https://docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/preview#heading=h.nso4gcezn7n1
+        self._write_event({
+            "name": task.get_name().strip(),
+            "cat": "runner",
+            "id": abs(hash(uuid)),
+            "ph": "E",  # End
+            "ts": time.time_ns() / 1000,
+            "pid": self._hosts[result._host._uuid].pid,
+            "args": {
+                "status": status,
+            },
+        })
+
+    def v2_runner_on_ok(self, result):
+        self._end_span(result, status="ok")
+
+    def v2_runner_on_unreachable(self, result):
+        self._end_span(result, 'unreachable')
+
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        self._end_span(result, status='failed')
+
+    def v2_runner_on_skipped(self, result):
+        self._end_span(result, status='skipped')
+
+    def v2_playbook_on_stats(self, stats):
+        self._f.close()
+
+@dataclass
+class Host:
+    name: str
+    pid: int
